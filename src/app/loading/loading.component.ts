a62f5aa5d2b71b27231dd801c9f40a0ce4aa7253
np1goal <np1goal@gmail.com>
2020-09-01 23:17:19 -0700

    Loading + Favicon
    Added a new loading with brain svg + Favicon changed

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoadingComponent implements OnInit {
 
+  loadingPercent: number = 20;
+
   constructor() { }
 
   ngOnInit(): void {
+    var _this = this;
+    document.getElementById('loading-number').textContent = _this.loadingPercent + ' %';
+    setInterval(() => {
+        if(_this.loadingPercent+20 <= 100) {
+          _this.loadingPercent = _this.loadingPercent + 20;
+          document.getElementById('loading-number').textContent = _this.loadingPercent + ' %';
+        } else {
+          clearInterval(1000);
+        }
+    }, 1000);
   }
 
+  increasePercentage() {
+    // this.loadingPercent = this.loadingPercent + 1;
+    // console.log(this.loadingPercent + 1);
+    // document.getElementById('loading-number').textContent = String(this.loadingPercent);
+  }
 }

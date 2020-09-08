80cebcc5c338c6d709102e1f1c2bc73d5ad62c2f
np1goal <np1goal@gmail.com>
2020-09-07 23:29:33 -0700

    Skills page
    Added text on skills page

--- src/app/skill/skill.component.ts
+++ src/app/skill/skill.component.ts
@@ -9,12 +9,12 @@ export class SkillComponent implements OnInit {
 
   activeTopic = "html";
   topicDescription = {
-    "html" : {topic: "HTML", angle: "rotateY(360deg)", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
-    "css" : {topic: "CSS", angle: "rotateY(90deg)", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
-    "javascript" :{topic: "Javascript", angle: "rotateY(180deg)", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
-    "angular" : {topic:"Angular", angle: "rotateY(270deg)", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
-    "scsssass" : {topic: "SCSS/SASS", angle: "rotateX(-90deg)", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
-    "threejs" : {topic: "Three.js", angle: "rotateX(90deg)", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
+    "html" : {topic: "HTML", angle: "rotateY(360deg)", description: "I would rank my HTML proficiency to be 95%. I know how to work with a simple HTML file to use it in a framework like Angular. Here are a few HTML concepts that I frequently work with:", conceptList: "Classes, Ids, Links, SVG, Lists, Entities, Layout"},
+    "css" : {topic: "CSS", angle: "rotateY(90deg)", description: "I would rank my CSS proficiency to be 95%. I am familiar with the design, layout, and display of web apps for different devices and screen sizes using CSS. Here are a few CSS concepts that I frequently use:", conceptList: "Box Model, Borders, Colors and Gradients, Transitions, Animations, CSS Flexbox, CSS Grids, Mobile Responsiveness"},
+    "javascript" :{topic: "Javascript", angle: "rotateY(180deg)", description: "I would rank my Javascript proficiency to be 90%. I have mostly used Javascript to work with data on web apps. Here are a few Javascript concepts that I frequently use to make web apps:", conceptList: "Javascript Arrays, Javascript Objects, Javascript Functions, HTML DOM, Browser BOM, JS JSON"},
+    "angular" : {topic:"Angular", angle: "rotateY(270deg)", description: "I would rank my Angular proficiency to be 80%. I use the Angular framework to create efficient web apps. Here are a few Javascript concepts that I frequently use to make web apps:", conceptList: "Angular CLI, Template Syntax, Components, Services, Directives, Routing"},
+    "scsssass" : {topic: "SCSS/SASS", angle: "rotateX(-90deg)", description: "I would rank my Angular proficiency to be 75%. SASS/SCSS makes some concepts not possible in CSS possible. Here are a few concepts that I use frequently:", conceptList: "Nesting, Functions, Control Directives, @mixin, @extend"},
+    "threejs" : {topic: "Three.js", angle: "rotateX(90deg)", description: "I would rank my Angular proficiency to be 40%. It is a new skill that I am presently learning. Here are a few concepts that I have learned:", conceptList: "Scenes, Camera, Renderer, Geometry, Animation"}
   };
 
   constructor() { }
@@ -35,7 +35,20 @@ export class SkillComponent implements OnInit {
 
   descriptionDisplay(topic) {
     document.getElementById('skill-topic').innerHTML = this.topicDescription[topic].topic;
-    document.getElementById('skill-description').innerHTML = this.topicDescription[topic].description;
+    var description = this.topicDescription[topic].description.split('.');
+    console.log(description);
+    var styledDescription = '';
+    for(var i = 0; i < description.length; i++) {
+      styledDescription = styledDescription + '- ' + description[i] + '<br><br>';
+    }
+    document.getElementById('skill-description').innerHTML = styledDescription;
+    var concepts = this.topicDescription[topic].conceptList.split(',');
+    var styledConcepts = '';
+    for(var i = 0; i < concepts.length; i++) {
+      styledConcepts = styledConcepts + '- ' + concepts[i] + '<br>';
+    }
+    document.getElementById('skill-concepts').innerHTML = styledConcepts;
+    document.getElementById('skill-concepts').style.marginLeft = '10%';
     document.getElementById('cube').style.transition = 'all 0.5s ease';
     document.getElementById('cube').style.transform = this.topicDescription[topic].angle;
   }

83dd9bcc925892505ab119916b1259ed59e9986e
np1goal <np1goal@gmail.com>
2020-09-11 23:36:07 -0700

    Projects
    Projects New Layout

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,7 +10,7 @@ export class ProjectsComponent implements OnInit {
   projectData = {
     0: { deg: -90, name: "Tutorial", src: "../../assets/tutorial.png", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.'},
     1: { deg: -180 ,name: "Aditi", src: "../../assets/aditi.png", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.'},
-    2: { deg: -270, name: "Animations", src: "../../assets/aditi.png", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.'},
+    2: { deg: -270, name: "Animations", src: "../../assets/animations.png", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.'},
     3: { deg: -360, name: "Portfolio", src: "../../assets/portfolio.png", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.'}
   }
 
@@ -18,29 +18,18 @@ export class ProjectsComponent implements OnInit {
 
   ngOnInit(): void {
     this.nextProject();
-    if (window.matchMedia("(max-width: 900px)").matches || window.matchMedia("(max-width: 600px)").matches) {
-      document.getElementById('project-img0-horizontal').setAttribute('src', this.projectData[0].src);
-      document.getElementById('project-img1-horizontal').setAttribute('src', this.projectData[1].src);
-      document.getElementById('project-img1-horizontal').style.transform = 'rotateZ('+this.projectData[1].deg.toString()+'deg)';
-      document.getElementById('project-img1-horizontal').style.opacity = '0';
-      document.getElementById('project-img2-horizontal').setAttribute('src', this.projectData[2].src);
-      document.getElementById('project-img2-horizontal').style.transform = 'rotateZ('+this.projectData[2].deg.toString()+'deg)';
-      document.getElementById('project-img2-horizontal').style.opacity = '0';
-      document.getElementById('project-img3-horizontal').setAttribute('src', this.projectData[3].src);
-      document.getElementById('project-img3-horizontal').style.transform = 'rotateZ('+this.projectData[3].deg.toString()+'deg)';
-      document.getElementById('project-img3-horizontal').style.opacity = '0';
-    } else {
-      document.getElementById('project-img0-vertical').setAttribute('src', this.projectData[0].src);
-      document.getElementById('project-img1-vertical').setAttribute('src', this.projectData[1].src);
-      document.getElementById('project-img1-vertical').style.transform = 'rotateZ('+this.projectData[1].deg.toString()+'deg)';
-      document.getElementById('project-img1-vertical').style.opacity = '0';
-      document.getElementById('project-img2-vertical').setAttribute('src', this.projectData[2].src);
-      document.getElementById('project-img2-vertical').style.transform = 'rotateZ('+this.projectData[2].deg.toString()+'deg)';
-      document.getElementById('project-img2-vertical').style.opacity = '0';
-      document.getElementById('project-img3-vertical').setAttribute('src', this.projectData[3].src);
-      document.getElementById('project-img3-vertical').style.transform = 'rotateZ('+this.projectData[3].deg.toString()+'deg)';
-      document.getElementById('project-img3-vertical').style.opacity = '0';
-    }
+      document.getElementById('project-img0').setAttribute('src', this.projectData[0].src);
+      document.getElementById('project-img0').style.transform = 'rotateZ('+this.projectData[0].deg.toString()+'deg)';
+      document.getElementById('project-img0').style.opacity = '1';
+      document.getElementById('project-img1').setAttribute('src', this.projectData[1].src);
+      document.getElementById('project-img1').style.transform = 'rotateZ('+this.projectData[1].deg.toString()+'deg)';
+      document.getElementById('project-img1').style.opacity = '0';
+      document.getElementById('project-img2').setAttribute('src', this.projectData[2].src);
+      document.getElementById('project-img2').style.transform = 'rotateZ('+this.projectData[2].deg.toString()+'deg)';
+      document.getElementById('project-img2').style.opacity = '0';
+      document.getElementById('project-img3').setAttribute('src', this.projectData[3].src);
+      document.getElementById('project-img3').style.transform = 'rotateZ('+this.projectData[3].deg.toString()+'deg)';
+      document.getElementById('project-img3').style.opacity = '0';
   }
 
   nextProject() {
@@ -50,21 +39,12 @@ export class ProjectsComponent implements OnInit {
         document.getElementById('previous-button').style.opacity = '1';
         document.getElementById('next-button').style.opacity = '1';
         this.projectData[i].deg = this.projectData[i].deg + 90;
-        if (window.matchMedia("(max-width: 900px)").matches || window.matchMedia("(max-width: 600px)").matches) { //Check for browser width
-          this.projectLayoutStyling('project-description'+i.toString()+'-vertical', 'project-title'+i.toString()+'-vertical', 'project-text'+i.toString()+'-vertical', 'project-img'+i.toString()+'-vertical');
           if (Math.abs(this.projectData[i].deg) === 0) {
-            this.projectStyling('project-description'+i.toString()+'-horizontal', '1', '60vw', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', 'all 1s ease', 'project-title'+i.toString()+'-horizontal', this.projectData[i].name, 'project-text'+i.toString()+'-horizontal', this.projectData[i].description, 'project-img'+i.toString()+'-horizontal', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', '1', 'all 1s ease');
+            this.projectStyling('project-description'+i.toString(), '1', '80%', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', 'all 1s ease', 'project-title'+i.toString(), this.projectData[i].name, 'project-text'+i.toString(), this.projectData[i].description, 'project-img'+i.toString(), 'rotateZ('+this.projectData[i].deg.toString()+'deg)', '1', 'all 1s ease');
           } else {
-            this.projectStyling('project-description'+i.toString()+'-horizontal', '0', '10vw', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', 'all 1s', 'project-title'+i.toString()+'-horizontal', '', 'project-text'+i.toString()+'-horizontal', '', 'project-img'+i.toString()+'-horizontal', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', '0', 'all 1s');
+            this.projectStyling('project-description'+i.toString(), '0', '10%', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', 'all 1s', 'project-title'+i.toString(), '', 'project-text'+i.toString(), '', 'project-img'+i.toString(), 'rotateZ('+this.projectData[i].deg.toString()+'deg)', '0', 'all 1s');
           }
-        } else {
-          this.projectLayoutStyling('project-description'+i.toString()+'-horizontal', 'project-title'+i.toString()+'-horizontal', 'project-text'+i.toString()+'-horizontal', 'project-img'+i.toString()+'-horizontal');
-          if (Math.abs(this.projectData[i].deg) === 0) {
-            this.projectStyling('project-description'+i.toString()+'-vertical', '1', '40vw', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', 'all 1s ease', 'project-title'+i.toString()+'-vertical', this.projectData[i].name, 'project-text'+i.toString()+'-vertical', this.projectData[i].description, 'project-img'+i.toString()+'-vertical', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', '1', 'all 1s ease');
-          } else {
-            this.projectStyling('project-description'+i.toString()+'-vertical', '0', '10vw', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', 'all 1s', 'project-title'+i.toString()+'-vertical', '', 'project-text'+i.toString()+'-vertical', '', 'project-img'+i.toString()+'-vertical', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', '0', 'all 1s');
-          }
-        }
+        
       } else {
         document.getElementById('next-button').style.opacity = '0.5';
       }
@@ -78,21 +58,11 @@ export class ProjectsComponent implements OnInit {
         document.getElementById('previous-button').style.opacity = '1';
         document.getElementById('next-button').style.opacity = '1';
         this.projectData[i].deg = this.projectData[i].deg - 90;
-        if (window.matchMedia("(max-width: 900px)").matches || window.matchMedia("(max-width: 600px)").matches) { //Check for browser width
-          this.projectLayoutStyling('project-description'+i.toString()+'-vertical', 'project-title'+i.toString()+'-vertical', 'project-text'+i.toString()+'-vertical', 'project-img'+i.toString()+'-vertical');
           if (Math.abs(this.projectData[i].deg) === 0) {
-            this.projectStyling('project-description'+i.toString()+'-horizontal', '1', '60vw', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', 'all 1s ease', 'project-title'+i.toString()+'-horizontal', this.projectData[i].name, 'project-text'+i.toString()+'-horizontal', this.projectData[i].description, 'project-img'+i.toString()+'-horizontal', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', '1', 'all 1s ease');
+            this.projectStyling('project-description'+i.toString(), '1', '80%', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', 'all 1s ease', 'project-title'+i.toString(), this.projectData[i].name, 'project-text'+i.toString(), this.projectData[i].description, 'project-img'+i.toString(), 'rotateZ('+this.projectData[i].deg.toString()+'deg)', '1', 'all 1s ease');
           } else {
-            this.projectStyling('project-description'+i.toString()+'-horizontal', '0', '10vw', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', 'all 1s', 'project-title'+i.toString()+'-horizontal', '', 'project-text'+i.toString()+'-horizontal', '', 'project-img'+i.toString()+'-horizontal', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', '0', 'all 1s');
+            this.projectStyling('project-description'+i.toString(), '0', '10%', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', 'all 1s', 'project-title'+i.toString(), '', 'project-text'+i.toString(), '', 'project-img'+i.toString(), 'rotateZ('+this.projectData[i].deg.toString()+'deg)', '0', 'all 1s');
           }
-        } else {
-          this.projectLayoutStyling('project-description'+i.toString()+'-horizontal', 'project-title'+i.toString()+'-horizontal', 'project-text'+i.toString()+'-horizontal', 'project-img'+i.toString()+'-horizontal');
-          if (Math.abs(this.projectData[i].deg) === 0) {
-            this.projectStyling('project-description'+i.toString()+'-vertical', '1', '40vw', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', 'all 1s ease', 'project-title'+i.toString()+'-vertical', this.projectData[i].name, 'project-text'+i.toString()+'-vertical', this.projectData[i].description, 'project-img'+i.toString()+'-vertical', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', '1', 'all 1s ease');
-          } else {
-            this.projectStyling('project-description'+i.toString()+'-vertical', '0', '10vw', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', 'all 1s', 'project-title'+i.toString()+'-vertical', '', 'project-text'+i.toString()+'-vertical', '', 'project-img'+i.toString()+'-vertical', 'rotateZ('+this.projectData[i].deg.toString()+'deg)', '0', 'all 1s');
-          }
-        }
       } else {
         document.getElementById('previous-button').style.opacity = '0.5';
       }
@@ -113,7 +83,8 @@ export class ProjectsComponent implements OnInit {
     document.getElementById(projectDescriptionId).style.transition = projectDescriptionTransition; 
     document.getElementById(projectTitleId).textContent = projectTitleName;
     document.getElementById(projectTitleId).style.fontWeight = 'bold';
-    document.getElementById(projectTitleId).style.fontSize = '1.2em';
+    document.getElementById(projectTitleId).style.paddingBottom = '5px';
+    document.getElementById(projectTitleId).style.fontSize = '1.5em';
     document.getElementById(projectTextId).textContent = projectTextDescription;
     document.getElementById(projectImgId).style.transform = projectImgTransform;
     document.getElementById(projectImgId).style.opacity = projectImgOpacity;
--- src/app/skill/skill.component.ts
+++ src/app/skill/skill.component.ts
@@ -9,12 +9,12 @@ export class SkillComponent implements OnInit {
 
   activeTopic = "html";
   topicDescription = {
-    "html" : {topic: "HTML", angle: "rotateY(360deg)", description: "I would rank my HTML proficiency to be 95%. I know how to work with a simple HTML file to use it in a framework like Angular. Here are a few HTML concepts that I frequently work with:", conceptList: "Classes, Ids, Links, SVG, Lists, Entities, Layout"},
-    "css" : {topic: "CSS", angle: "rotateY(90deg)", description: "I would rank my CSS proficiency to be 95%. I am familiar with the design, layout, and display of web apps for different devices and screen sizes using CSS. Here are a few CSS concepts that I frequently use:", conceptList: "Box Model, Borders, Colors and Gradients, Transitions, Animations, CSS Flexbox, CSS Grids, Mobile Responsiveness"},
-    "javascript" :{topic: "Javascript", angle: "rotateY(180deg)", description: "I would rank my Javascript proficiency to be 90%. I have mostly used Javascript to work with data on web apps. Here are a few Javascript concepts that I frequently use to make web apps:", conceptList: "Javascript Arrays, Javascript Objects, Javascript Functions, HTML DOM, Browser BOM, JS JSON"},
-    "angular" : {topic:"Angular", angle: "rotateY(270deg)", description: "I would rank my Angular proficiency to be 80%. I use the Angular framework to create efficient web apps. Here are a few Javascript concepts that I frequently use to make web apps:", conceptList: "Angular CLI, Template Syntax, Components, Services, Directives, Routing"},
-    "scsssass" : {topic: "SCSS/SASS", angle: "rotateX(-90deg)", description: "I would rank my Angular proficiency to be 75%. SASS/SCSS makes some concepts not possible in CSS possible. Here are a few concepts that I use frequently:", conceptList: "Nesting, Functions, Control Directives, @mixin, @extend"},
-    "threejs" : {topic: "Three.js", angle: "rotateX(90deg)", description: "I would rank my Angular proficiency to be 40%. It is a new skill that I am presently learning. Here are a few concepts that I have learned:", conceptList: "Scenes, Camera, Renderer, Geometry, Animation"}
+    "html" : {topic: "HTML", angle: "rotateY(360deg)", description: "I know how to work with a simple HTML file to use it in a framework like Angular. Here are a few HTML concepts that I frequently work with:", conceptList: "Classes, Ids, Links, SVG, Lists, Entities, Layout"},
+    "css" : {topic: "CSS", angle: "rotateY(90deg)", description: "I am familiar with the design, layout, and display of web apps for different devices and screen sizes using CSS. Here are a few CSS concepts that I frequently use:", conceptList: "Box Model, Borders, Colors and Gradients, Transitions, Animations, CSS Flexbox, CSS Grids, Mobile Responsiveness"},
+    "javascript" :{topic: "Javascript", angle: "rotateY(180deg)", description: "I have mostly used Javascript to work with data on web apps. Here are a few Javascript concepts that I frequently use to make web apps:", conceptList: "Javascript Arrays, Javascript Objects, Javascript Functions, HTML DOM, Browser BOM, JS JSON"},
+    "angular" : {topic:"Angular", angle: "rotateY(270deg)", description: "I use the Angular framework to create efficient web apps. Here are a few Javascript concepts that I frequently use to make web apps:", conceptList: "Angular CLI, Template Syntax, Components, Services, Directives, Routing"},
+    "scsssass" : {topic: "SCSS/SASS", angle: "rotateX(-90deg)", description: "SASS/SCSS makes some concepts not possible in CSS possible. Here are a few concepts that I use frequently:", conceptList: "Nesting, Functions, Control Directives, @mixin, @extend"},
+    "threejs" : {topic: "Three.js", angle: "rotateX(90deg)", description: "It is a new skill that I am presently learning. Here are a few concepts that I have learned:", conceptList: "Scenes, Camera, Renderer, Geometry, Animation"}
   };
 
   constructor() { }
@@ -24,9 +24,9 @@ export class SkillComponent implements OnInit {
   }
 
   changeTab(topic) {
-    document.getElementById(this.activeTopic).style.color = '#302718';
+    document.getElementById(this.activeTopic).style.color = '#c9e4d7';
     document.getElementById(this.activeTopic).style.animation = 'topic-border-remove-animation 0.1s forwards';
-    document.getElementById(topic).style.color = '#009F55';
+    document.getElementById(topic).style.color = '#7bffc1';
     document.getElementById(topic).style.animation = 'topic-border-animation 0.1s forwards';
 
     this.activeTopic = topic;
